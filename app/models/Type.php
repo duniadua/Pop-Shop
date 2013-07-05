9dd88d0ce40d8ef25f196a59ece50ff4eba52dc7
duniadua <duniadua@gmail.com>
2013-07-05 19:37:12 +0700

    Type Model

---
+++ app/models/Type.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Type
+ *
+ * @author sahid
+ */
+class Type extends Eloquent {
+    //put your code here
+    protected $fillable = array(
+        'name',
+        'details',
+        'create_by',        
+        'ip_address',
+        'active'
+    );
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'payment_type';
+}
