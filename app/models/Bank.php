95bb35799db3fc3480a9129a6a81036cc9ba9d8d
duniadua <duniadua@gmail.com>
2013-07-05 18:58:58 +0700

    create Bank Model

---
+++ app/models/Bank.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Bank
+ *
+ * @author sahid
+ */
+class Bank extends Eloquent {
+    //put your code here
+     protected $fillable = array(
+        'name',
+        'details',
+        'create_by',        
+        'ip_address',
+        'active'
+    );
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'banks';
+
+}
