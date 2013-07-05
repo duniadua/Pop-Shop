233f0e3573b071f311c336c40ba5838b5cdf1dda
duniadua <duniadua@gmail.com>
2013-07-05 19:36:05 +0700

    landing page type

---
+++ app/views/type/index.php
@@ -0,0 +1,49 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title><?php echo $title ?></title>
+        <?php
+        echo $bootstrap;
+        echo $header;
+
+        $mssg = Session::get('mssg');
+        ?>
+    </head>
+    <body>
+        <div class="container well">
+            <div class="span5">
+                <fieldset>
+                    <legend><?php echo $page_title ?></legend>
+                    <label>Name</label>
+                    <?php
+                    echo Form::open(array('action' => 'TypeController@store'));
+                    echo Form::text('name');
+                    echo Form::label('details', 'Details');
+                    echo Form::text('details') . "</br>";
+
+                    $active = array(
+                        1 => 'active',
+                        0 => 'inactive',
+                    );
+
+                    echo Form::select('active', $active) . "</br>";
+                    echo Form::submit('Save', array('class' => 'btn btn-inverse'));
+                    echo Form::close();
+                    echo $mssg;
+                    ?>
+                </fieldset>
+            </div>
+            <div class="span5">
+
+            </div>
+        </div>
+        <?php
+        echo $footer;
+        ?>
+    </body>
+</html>
