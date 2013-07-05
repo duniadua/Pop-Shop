743871e89833e524697f0b94a3b8b067ee57d97a
duniadua <duniadua@gmail.com>
2013-07-05 19:36:56 +0700

    Status Model

---
+++ app/models/Status.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Status
+ *
+ * @author sahid
+ */
+class Status extends Eloquent {
+    //put your code here
+    protected $fillable = array(
+        'name',
+        'details',
+        'create_by',        
+        'ip_address',
+        'active'
+    );
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'payment_status';
+
+}
