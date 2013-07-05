b224e3576a98996a6d760e8407de7932f6b17ad6
duniadua <duniadua@gmail.com>
2013-07-05 19:38:12 +0700

    Add function

--- app/controllers/TypeController.php
+++ app/controllers/TypeController.php
@@ -2,78 +2,96 @@
 
 class TypeController extends \BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index() {
+        //
+        $data = array(
+            'title' => 'POP Shop - New Payment Type',
+            'page_title' => 'Add Payment Type'
+        );
+        return View::make('type.index', $data)
+                        ->nest('header', 'asset.header_cp')
+                        ->nest('bootstrap', 'asset.html_config_common')
+                        ->nest('footer', 'asset.footer');
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create() {
+        //
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store() {
+        //
+        try {
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+            $data = array(
+                'name' => Input::get('name'),
+                'details' => Input::get('details'),
+                'create_by' => 'I',
+                'ip_address' => Request::getClientIp(),
+                'active' => Input::get('active'),
+            );
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+            $type = new Type($data);
+            $type->save();
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+            return Redirect::to('type')->with('mssg', '<div class=alert> ' . Input::get('name') . ' Added </div>');
+        } catch (Exception $exc) {
+            echo $exc->getMessage();
+        }
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id) {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id) {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id) {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id) {
+        //
+    }
 
 }
