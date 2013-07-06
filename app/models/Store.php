8e7ff7de3ebe036c38f1de7c3bdce722fa3a5e04
duniadua <duniadua@gmail.com>
2013-07-06 11:13:56 +0700

    new Model

---
+++ app/models/Store.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Store
+ *
+ * @author sahid
+ */
+class Store extends Eloquent {
+    
+    //put your code here
+    protected $fillable = array(                
+        'name',
+        'email',        
+        'address',
+        'address2',
+        'address3',
+        'postcode',
+        'city',
+        'province',
+        'home_no',
+        'mobile_no',        
+        'ip_address',
+        'active'
+    );
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'store';
+
+}
+
