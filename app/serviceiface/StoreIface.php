be9a524e4c010cc31a2e99d7308db45acdd6eb1e
duniadua <duniadua@gmail.com>
2013-07-07 23:45:31 +0700

    Store interface untuk nambah record

---
+++ app/serviceiface/StoreIface.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sahid
+ */
+interface StoreIface {
+    //put your code here
+    public function insertStore($input);
+}
+
+?>
