9347dae8502bae3d77ebad2abd3fe3b43d7e6440
duniadua <duniadua@gmail.com>
2014-02-21 18:29:48 +0700

    Adding API Functionality for Ajax Purpose

---
+++ app/controllers/API2Controller.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of API2Controller
+ *
+ * @author sahid
+ */
+class API2Controller extends BaseController{
+    //put your code here
+    /**
+     * Show the courier list
+     */
+    public function getCourier($id){
+        
+        
+    }
+    
+}
+
+?>
